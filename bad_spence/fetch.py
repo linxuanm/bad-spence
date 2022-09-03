6b7b71e8f1cd247f9fda9d970e46659972dd5584
BuildTools <davidmaamoaix@gmail.com>
2022-09-03 22:15:25 +0800

    finished

--- bad_spence/fetch.py
+++ bad_spence/fetch.py
@@ -1,17 +1,17 @@
 import requests
 
+
 def fetch_citation(link):
-    headers = {
-        'Accept': 'text/x-bibliography',
-        'style': 'modern-language-association-8th-edition'
+    data = {
+        'doi': link,
+        'style': 'modern-language-association-8th-edition',
+        'lang': 'en-US'
     }
 
-    resp = requests.get(link, headers=headers)
+    resp = requests.get('https://citation.crosscite.org/format', params=data)
     if resp.status_code != 200:
         raise ValueError(
             f'bad request when handling {link}: code {resp.status_code}'
         )
 
-    return resp.text
-
-fetch_citation('https://doi.org/10.48550/arXiv.1910.00935')
+    return (link, resp.text.strip())
--- bad_spence/generator.py
+++ bad_spence/generator.py
@@ -1,4 +1,5 @@
 import argparse
+from pqdm.threads import pqdm
 
 from .fetch import fetch_citation
 
@@ -37,4 +38,13 @@ def main():
                 f'{link} does not begin with "10" (format not yet supported)'
             )
 
-        links.append(f'https://doi.org/{link}')
+        links.append(link)
+
+    htmls = pqdm(links, fetch_citation, n_jobs=4)
+
+    if sort == 'none':
+        htmls = sorted(htmls, key=lambda x, l=links: l.index(x[0]))
+    else:
+        htmls = sorted(htmls, key=lambda x: x[1])
+
+    print('\n\n'.join(i[1] for i in htmls))
